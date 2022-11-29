d6ba6e535853f1b78fdfd14482e2337acb609c2d
zeon-kun <zeonkunix@gmail.com>
2022-11-29 19:31:04 +0700

    feat : add laravel

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home', [
+        "title" => "Home",
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About",
+        "name" => "Muhammad Rafif Tri Risqullah",
+        "email" => "zeonkunix@gmail.com",
+        "image" => "DSC_0184.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts', [
+        "title" => "Posts"
+    ]);
+});
